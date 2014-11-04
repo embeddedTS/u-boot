849bb8e135ae705d6e550024f4cb91591bb95f0f
Mark Featherston <mark@embeddedarm.com>
2014-11-04 11:27:12 -0700

    Added commands required for TS-4900 & TS-7970 ice40 for programming the onboard ice40 sram bbdetect for the 4900 to detect the baseboard id

---
+++ common/cmd_bbdetect.c
@@ -0,0 +1,89 @@
+#include <common.h>
+#include <command.h>
+#include <asm/gpio.h>
+
+static int do_bbdetect(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	int i, id = 0;
+	gpio_direction_output(CONFIG_BB_S0, 1);
+	gpio_direction_output(CONFIG_BB_S1, 1);
+	gpio_direction_output(CONFIG_BB_S2, 1);
+	gpio_direction_input(CONFIG_BB_IN);
+
+	for(i = 0; i < 8; i++) {
+		if(i & 1)gpio_set_value(CONFIG_BB_S0, 1);
+		else gpio_set_value(CONFIG_BB_S0, 0);
+
+		if(i & 2)gpio_set_value(CONFIG_BB_S1, 1);
+		else gpio_set_value(CONFIG_BB_S1, 0);
+		
+		if(i & 4)gpio_set_value(CONFIG_BB_S2, 1);
+		else gpio_set_value(CONFIG_BB_S2, 0);
+
+		udelay(CONFIG_BB_USDLY);
+
+		id = (id >> 1);
+		if(gpio_get_value(CONFIG_BB_IN)) id |= 0x80;
+	}
+
+	switch (id & ~0xc0) {
+		case 0:
+			setenv("baseboard", "8200");
+			break;
+		case 2:
+			setenv("baseboard", "8390");
+			break;
+		case 4:
+			setenv("baseboard", "8500");
+			break;
+		case 5:
+			setenv("baseboard", "8400");
+			break;
+		case 6:
+			setenv("baseboard", "8160");
+			break;
+		case 7:
+			setenv("baseboard", "8100");
+			break;
+		case 8:
+			setenv("baseboard", "8820");
+			break;
+		case 9:
+			setenv("baseboard", "8150");
+			break;
+		case 10:
+			setenv("baseboard", "8900");
+			break;
+		case 11:
+			setenv("baseboard", "8290");
+			break;
+		case 13:
+			setenv("baseboard", "8700");
+			break;
+		case 14:
+			setenv("baseboard", "8280");
+			break;
+		case 15:
+			setenv("baseboard", "8380");
+			break;
+		case 16:
+			setenv("baseboard", "an20");
+			break;
+		case 17:
+			setenv("baseboard", "8920");
+			break;
+		default:
+			setenv("baseboard", "unknown");
+			break;
+	}
+
+	setenv_hex("baseboardid", id & ~0xc0);
+	setenv_hex("rev", ((id & 0xc0) >> 6));
+	return 0;
+}
+
+U_BOOT_CMD(
+	bbdetect,	1,		1,	do_bbdetect,
+	"Reads the value from a hard wired 8-input mux",
+	""
+);
---
+++ common/cmd_ice40.c
@@ -0,0 +1,56 @@
+#include <common.h>
+#include <command.h>
+#include <asm/gpio.h>
+#include <spi.h>
+
+static int do_ice40_load(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	unsigned int len, data;
+	struct spi_slave *slave;
+	int ret_val, i;
+
+	// Parse image from mkimage
+	data = simple_strtoul(argv[1], NULL, 16);
+	len = simple_strtoul(argv[2], NULL, 16);
+
+	slave = spi_setup_slave(1, CONFIG_ICE40_BUS, 25000000, SPI_MODE_0);
+	if(spi_claim_bus(slave)){
+		printf("Failed to claim the SPI bus\n");
+		return 1;
+	}
+
+	gpio_direction_input(CONFIG_ICE40_FPGA_DONE); // fpga_done
+	gpio_direction_output(CONFIG_ICE40_FPGA_RESET, 0); // reset low
+	gpio_direction_output(CONFIG_ICE40_CS, 0); // spi cs# low
+	gpio_set_value(CONFIG_ICE40_FPGA_RESET, 1); // reset high
+	udelay(800);
+
+	ret_val = spi_xfer(slave, len * 8, (void *)data, NULL, 0);
+
+	// FPGA requires additional spi clocks after bitstream
+	char zeroes[100];
+	memset(zeroes, 0, 100);
+	ret_val = spi_xfer(slave, 100 * 8, zeroes, NULL, 0);
+
+	gpio_set_value(CONFIG_ICE40_CS, 1); // spi cs# high
+
+	for(i = 0; i <= 3000; i++)
+	{
+		if(gpio_get_value(CONFIG_ICE40_FPGA_DONE))
+			break;
+		if(i == 3000){ 
+			printf("FPGA_DONE never asserted\n");
+			ret_val = 1;
+		}
+		udelay(1000);
+	}
+
+	spi_release_bus(slave);
+
+	return ret_val;
+}
+
+U_BOOT_CMD(ice40, 3, 0, do_ice40_load,
+	"ICE40 programming support",
+	" [image address] [filesize]\n"
+);
