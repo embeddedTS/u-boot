0471155c1e7f643d7da310028d6bc7bbfd73051e
Mark Featherston <mark@embeddedarm.com>
2015-09-15 15:20:04 -0700

    Added mx28 build script

---
+++ build-mx28.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabi-
+export DATE=$(date +"%b-%d-%Y")
+
+FAIL=0
+
+rm -rf out
+mkdir out > /dev/null 2>&1
+
+make ts7680_defconfig
+make -j9 u-boot.sb
+if [ $? -eq 0 ]; then
+	cp u-boot.sb out/ts7680-"$DATE".sb
+else
+	let FAIL=FAIL+1
+fi
+
+if [ $FAIL != 0 ]; then
+	echo "$FAIL BUILDS FAILED.  DO NOT RELEASE"
+fi
