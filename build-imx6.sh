3502bbcd7b351cdf461a6548d425ce8ac8ca2386
Mark Featherston <mark@embeddedarm.com>
2017-04-27 08:51:16 -0700

    Use more consistent yocto toolchain than distributions toolchain

--- build-imx6.sh
+++ build-imx6.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -x
 
 export ARCH=arm
-export CROSS_COMPILE=arm-linux-gnueabihf-
+#export CROSS_COMPILE=arm-linux-gnueabihf-
+export CROSS_COMPILE=/opt/poky/2.2.1/sysroots/x86_64-pokysdk-linux/usr/bin/arm-poky-linux-gnueabi/arm-poky-linux-gnueabi-
 export DATE=$(date +"%b-%d-%Y")
 
 FAIL=0
