a022d6caea3ae84c5a2aa66889f9e30db8642569
Kris Bahnsen <kris@embeddedarm.com>
2019-04-24 12:39:57 -0700

    ts7670: Initial commit of files for TS-7670 support

---
+++ board/technologic/ts7670/ts7670.c
@@ -0,0 +1,184 @@
+/*
+ * Technologic TS-7670 Single-board Computer
+ *
+ * (C) Copyright 2019 Technologic Systems
+ * Based on work by:
+ * Stuart Longland of VRT Systems <stuartl@vrt.com.au>
+ *
+ * Based on mx28evk.c:
+ * Freescale MX28EVK board
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <asm/gpio.h>
+#include <asm/io.h>
+#include <asm/arch/imx-regs.h>
+#include <asm/arch/iomux-mx28.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/sys_proto.h>
+#include <linux/mii.h>
+#include <miiphy.h>
+#include <netdev.h>
+#include <errno.h>
+#include <spi.h>
+#include <fpga.h>
+#include <lattice.h>
+#include <i2c.h>
+
+#define TS7670_EN_SDPWR		MX28_PAD_PWM3__GPIO_3_28
+#define TS7670_SDBOOT_JP	MX28_PAD_LCD_D00__GPIO_1_0
+#define TS7670_ENET_RSTn	MX28_PAD_SSP0_DETECT__GPIO_2_9
+
+DECLARE_GLOBAL_DATA_PTR;
+int random_mac = 0;
+void hw_watchdog_init(void);
+
+void mx28_adjust_mac(int dev_id, unsigned char *mac)
+{
+	mac[0] = 0x00;
+	mac[1] = 0xD0;
+	mac[2] = 0x69;
+}
+
+
+int board_early_init_f(void)
+{
+	/* IO0 clock at 480MHz */
+	mxs_set_ioclk(MXC_IOCLK0, 480000);
+	/* IO1 clock at 480MHz */
+	mxs_set_ioclk(MXC_IOCLK1, 480000);
+
+	/* SSP clocks at 96MHz */
+	mxs_set_sspclk(MXC_SSPCLK0, 96000, 0);
+	/* SSP clocks at 96MHz */
+	mxs_set_sspclk(MXC_SSPCLK1, 96000, 0);
+	/* SSP2 clock at 160MHz */
+	mxs_set_sspclk(MXC_SSPCLK2, 160000, 0);
+
+	return 0;
+}
+
+int dram_init(void)
+{
+	return mxs_dram_init();
+}
+
+int misc_init_r(void)
+{
+	int sdboot = 0;
+
+	setenv("model", "7670");
+
+	gpio_direction_input(TS7670_SDBOOT_JP);
+	sdboot = gpio_get_value(TS7670_SDBOOT_JP);
+
+	if(sdboot) setenv("jpsdboot", "on");
+	else setenv("jpsdboot", "off");
+
+	hw_watchdog_init();
+
+	return 0;
+}
+
+int board_init(void)
+{
+	/* Adress of boot parameters */
+	gd->bd->bi_boot_params = PHYS_SDRAM_1 + 0x100;
+
+	return 0;
+}
+
+static int ts7670_mmc_cd(int id) {
+	return 1;
+}
+
+int board_mmc_init(bd_t *bis)
+{
+	int ret;
+	mxs_iomux_setup_pad(TS7670_EN_SDPWR);
+
+	gpio_direction_output(TS7670_EN_SDPWR, 1); // EN_SD_POWER#
+	udelay(1000);
+	gpio_direction_output(TS7670_EN_SDPWR, 0);
+
+	/* SD card */
+	ret = mxsmmc_initialize(bis, 0, NULL, ts7670_mmc_cd);
+	if(ret != 0) {
+		printf("SD controller initialized with %d\n", ret);
+	}
+
+	/* eMMC */
+	ret = mxsmmc_initialize(bis, 1, NULL, ts7670_mmc_cd);
+	if(ret != 0) {
+		printf("eMMC controller initialized with %d\n", ret);
+	}
+
+	return 0;
+}
+
+#ifdef	CONFIG_CMD_NET
+
+#define MXS_OCOTP_MAX_TIMEOUT   1000000
+
+int board_eth_init(bd_t *bis)
+{
+	struct mxs_clkctrl_regs *clkctrl_regs =
+		(struct mxs_clkctrl_regs *)MXS_CLKCTRL_BASE;
+	struct eth_device *dev;
+	struct mxs_ocotp_regs *ocotp_regs =
+	  (struct mxs_ocotp_regs *)MXS_OCOTP_BASE;
+	uint32_t data;
+
+	int ret;
+	uchar enetaddr[6];
+
+	ret = cpu_eth_init(bis);
+	if (ret)
+		return ret;
+
+	/* MX28EVK uses ENET_CLK PAD to drive FEC clock */
+	writel(CLKCTRL_ENET_TIME_SEL_RMII_CLK | CLKCTRL_ENET_CLK_OUT_EN,
+	       &clkctrl_regs->hw_clkctrl_enet);
+
+	ret = fecmxc_initialize_multi(bis, 0, 0, MXS_ENET0_BASE);
+	if (ret) {
+		puts("FEC MXS: Unable to init FEC0\n");
+		return ret;
+	}
+
+	dev = eth_get_dev_by_name("FEC0");
+	if (!dev) {
+		puts("FEC MXS: Unable to get FEC0 device entry\n");
+		return -EINVAL;
+	}
+
+	eth_parse_enetaddr(getenv("ethaddr"), enetaddr);
+	if (!enetaddr[3] && !enetaddr[4] && !enetaddr[5]) {
+		printf("No MAC addr. set in fuses.  Using random MAC addr.\n");
+
+		writel(OCOTP_CTRL_RD_BANK_OPEN, &ocotp_regs->hw_ocotp_ctrl_set);
+
+		if (mxs_wait_mask_clr(&ocotp_regs->hw_ocotp_ctrl_reg,
+		  OCOTP_CTRL_BUSY, MXS_OCOTP_MAX_TIMEOUT)) {
+			printf("MXS FEC: Can't get MAC from OCOTP\n");
+			return -EIO;
+		}
+
+		data = readl(&ocotp_regs->hw_ocotp_ops2);
+
+		enetaddr[3] = 0x4f;
+		enetaddr[4] = (data >> 8) & 0xff;
+		enetaddr[5] = data & 0xff;
+		mx28_adjust_mac(0, enetaddr);
+
+		if (eth_setenv_enetaddr("ethaddr", enetaddr)) {
+			printf("Failed to set ethernet address\n");
+		}
+	}
+
+	return ret;
+}
+
+#endif
---
+++ include/configs/ts7670.h
@@ -0,0 +1,196 @@
+/*
+ * Copyright (C) 2019 Technologic Systems
+ *
+ * TS-7670 config
+ * Based on m28evk.h
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef __CONFIGS_TS7670_H__
+#define __CONFIGS_TS7670_H__
+
+/* System configurations */
+#define CONFIG_MX28				/* i.MX28 SoC */
+
+/* U-Boot Commands */
+#define CONFIG_SYS_NO_FLASH
+#include <config_cmd_default.h>
+#define CONFIG_DISPLAY_CPUINFO
+#define CONFIG_DOS_PARTITION
+
+#define CONFIG_BOARD_SPECIFIC_LED
+#define CONFIG_STATUS_LED
+#define CONFIG_CMD_LED
+
+#define STATUS_LED_BOOT			STATUS_LED_GREEN
+
+#define CONFIG_RED_LED			MX28_PAD_GPMI_CE1N__GPIO_0_17
+#define CONFIG_GREEN_LED		MX28_PAD_GPMI_RESETN__GPIO_0_28
+#define CONFIG_YEL_LED			MX28_PAD_LCD_RS__GPIO_1_26
+#define CONFIG_BLUE_LED			MX28_PAD_LCD_RD_E__GPIO_1_24
+#define STATUS_LED_RED			0
+#define STATUS_LED_GREEN		1
+#define STATUS_LED_YELLOW		2
+#define STATUS_LED_BLUE			3
+
+#define STATUS_LED_BIT                  STATUS_LED_RED
+#define STATUS_LED_STATE                STATUS_LED_ON
+#define STATUS_LED_PERIOD               (CONFIG_SYS_HZ / 2)
+
+#define STATUS_LED_BIT1                 STATUS_LED_GREEN
+#define STATUS_LED_STATE1               STATUS_LED_OFF
+#define STATUS_LED_PERIOD1              (CONFIG_SYS_HZ / 2)
+
+#define STATUS_LED_BIT2                 STATUS_LED_YELLOW
+#define STATUS_LED_STATE2               STATUS_LED_OFF
+#define STATUS_LED_PERIOD2              (CONFIG_SYS_HZ / 2)
+
+#define STATUS_LED_BIT3                 STATUS_LED_BLUE
+#define STATUS_LED_STATE3               STATUS_LED_OFF
+#define STATUS_LED_PERIOD3              (CONFIG_SYS_HZ / 2)
+
+#define CONFIG_CMD_CACHE
+#define CONFIG_CMD_DHCP
+#define CONFIG_CMD_GPIO
+#define CONFIG_CMD_MII
+#define CONFIG_CMD_MMC
+#define CONFIG_CMD_FAT
+#define CONFIG_CMD_EXT2
+#define CONFIG_CMD_EXT3
+#define CONFIG_CMD_EXT4
+#define CONFIG_CMD_EXT4_WRITE
+#define CONFIG_CMD_FS_GENERIC
+#define CONFIG_CMD_NET
+#define CONFIG_CMD_NFS
+#define CONFIG_CMD_PING
+#define CONFIG_CMD_SAVEENV
+#define CONFIG_CMD_SETEXPR
+#define CONFIG_CMD_BOOTZ
+#define CONFIG_CMD_USB
+#define CONFIG_CMD_TIME
+#define CONFIG_LIB_RAND
+#define CONFIG_OF_LIBFDT
+#define CONFIG_CMD_BOOTZ
+#define CONFIG_SUPPORT_RAW_INITRD
+
+/* Memory configuration */
+#define CONFIG_SYS_ALT_MEMTEST
+#define CONFIG_CMD_MEMTEST
+
+#define CONFIG_NR_DRAM_BANKS		1		/* 1 bank of DRAM */
+#define PHYS_SDRAM_1			0x40000000	/* Base address */
+#define PHYS_SDRAM_1_SIZE		0x40000000	/* Max 1 GB RAM */
+#define CONFIG_SYS_SDRAM_BASE		PHYS_SDRAM_1
+
+/* Env is at the 1MB boundary in emmc boot partition 0 */
+#define CONFIG_ENV_IS_IN_MMC
+#define CONFIG_SYS_MMC_ENV_DEV		1 /* mmcblk0 */
+#define CONFIG_SYS_MMC_ENV_PART		1 /* boot0 */
+
+#define CONFIG_ENV_OFFSET		0x400000 /* 4MiB */
+#define CONFIG_ENV_SIZE			(128 * 1024) /* 128k */
+#define CONFIG_ENV_OFFSET_REDUND	0x600000 /* 6MiB */
+#define CONFIG_ENV_OVERWRITE
+
+#define CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
+
+/* FEC Ethernet on SoC */
+#ifdef	CONFIG_CMD_NET
+#define CONFIG_FEC_MXC
+#define CONFIG_NET_MULTI
+#define CONFIG_MX28_FEC_MAC_IN_OCOTP
+#endif
+
+/* USB */
+#ifdef	CONFIG_CMD_USB
+#define CONFIG_EHCI_MXS_PORT1
+#define CONFIG_USB_MAX_CONTROLLER_COUNT	1
+#define	CONFIG_USB_STORAGE
+#endif
+
+/* I2C */
+#define CONFIG_CMD_I2C
+#define CONFIG_I2C_MXS
+#define CONFIG_SYS_I2C_SPEED		100000 
+
+#define CONFIG_LOADADDR			0x42000000
+#define CONFIG_SYS_LOAD_ADDR		CONFIG_LOADADDR
+#define CONFIG_MISC_INIT_R
+
+/* Watchdog support */
+#define CONFIG_TS7400V2_WATCHDOG
+#define CONFIG_HW_WATCHDOG
+#define CONFIG_SPL_WATCHDOG_SUPPORT
+
+
+/* Miscellaneous configurable options */
+#define CONFIG_SYS_LONGHELP
+#define CONFIG_SYS_HUSH_PARSER
+#define CONFIG_SYS_PROMPT		"U-Boot > "
+#define CONFIG_AUTO_COMPLETE
+
+#define CONFIG_BOOTDELAY		1
+#define CONFIG_AUTOBOOT_KEYED		1
+#define CONFIG_AUTOBOOT_PROMPT         "Press Ctrl+C to abort autoboot in %d second(s)\n", bootdelay
+#define CTRL(c) ((c)&0x1F)     
+#define CONFIG_AUTOBOOT_STOP_STR       (char []){CTRL('C'), 0}
+
+/*#define CONFIG_PREBOOT \*/
+
+/* Extra Environment */
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"autoload=no\0" \
+	"nfsroot=/nfsroot/\0" \
+	"nfsip=192.168.0.1\0" \
+	"fdtaddr=0x41000000\0" \
+	"cmdline_append=rw rootwait console=ttyAMA0,115200 loglevel=3\0" \
+	"boot_fdt=yes\0" \
+	"ip_dyn=yes\0" \
+        "clearenv=mmc dev 1 1; mmc erase 2000 400; mmc erase 3000 400;\0" \
+	"emmcboot=echo Booting from the onboard eMMC  ...; " \
+		"if load mmc 1:2 ${loadaddr} /boot/boot.ub; " \
+			"then echo Booting from custom /boot/boot.ub; " \
+			"source ${loadaddr}; " \
+		"fi; " \
+		"load mmc 1:2 ${loadaddr} /boot/uImage; " \
+		"load mmc 1:2 ${fdtaddr} /boot/imx28-ts${model}.dtb; " \
+		"setenv bootargs root=/dev/mmcblk1p2 ${cmdline_append}; " \
+		"bootm ${loadaddr} - ${fdtaddr}; \0"\
+	"sdboot=echo Booting from the SD Card ...; " \
+		"if load mmc 0:2 ${loadaddr} /boot/boot.ub; " \
+			"then echo Booting from custom /boot/boot.ub; " \
+			"source ${loadaddr}; " \
+		"fi; " \
+		"load mmc 0:2 ${loadaddr} /boot/uImage; " \
+		"load mmc 0:2 ${fdtaddr} /boot/imx28-ts${model}.dtb; " \
+		"setenv bootargs root=/dev/mmcblk0p2 ${cmdline_append}; " \
+		"bootm ${loadaddr} - ${fdtaddr}; \0"\
+	"usbprod=usb start; " \
+		"if usb storage; " \
+			"then echo Checking USB storage for updates; " \
+			"if load usb 0:1 ${loadaddr} /tsinit.ub; " \
+				"then led green on;" \
+				"source ${loadaddr}; " \
+				"led red off; " \
+				"exit; " \
+			"fi; " \
+		"fi; \0" \
+	"nfsboot=echo Booting from NFS ...; " \
+		"dhcp; " \
+		"env set serverip ${nfsip}; " \
+		"nfs ${loadaddr} ${nfsroot}/boot/uImage; " \
+		"setenv bootargs root=/dev/nfs ip=dhcp " \
+		  "nfsroot=${serverip}:${nfsroot},vers=2,nolock ${cmdline_append}; " \
+		"nfs ${fdtaddr} ${nfsroot}/boot/imx28-ts${model}.dtb; " \
+		"bootm ${loadaddr} - ${fdtaddr};\0"\
+
+#define CONFIG_BOOTCOMMAND \
+	"if test ${jpsdboot} = 'on' ; " \
+		"then run sdboot; " \
+		"else run emmcboot; " \
+	"fi;"
+	
+/* The rest of the configuration is shared */
+#include <configs/mxs.h>
+
+#endif /* __CONFIGS_TS7670_H__ */
