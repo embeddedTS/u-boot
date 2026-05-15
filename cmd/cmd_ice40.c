587788af387c358442f36fc25993d077cef03056
Kris Bahnsen <kris@embeddedTS.com>
2026-05-15 15:20:41 -0700

    cmd_ice40: test to do chunked loading of FPGA with 32kb chunks and print between them
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- cmd/cmd_ice40.c
+++ cmd/cmd_ice40.c
@@ -13,16 +13,17 @@
 
 static int do_ice40_load(struct cmd_tbl *cmdtp, int flag, int argc, char * const argv[])
 {
-	unsigned int len, data;
+	ulong len, i;
+	u8 *data;
 	struct spi_slave *slave;
-	int ret_val, i;
+	int ret_val;
 
 	gpio_request(CONFIG_ICE40_FPGA_DONE, "ice40");
 	gpio_request(CONFIG_ICE40_FPGA_RESET, "ice40");
 	gpio_request(CONFIG_ICE40_CS, "ice40");
 
 	// Parse image from mkimage
-	data = simple_strtoul(argv[1], NULL, 16);
+	data = (u8 *)simple_strtoul(argv[1], NULL, 16);
 	len = simple_strtoul(argv[2], NULL, 16);
 
 	printf("Setting up bus\n");
@@ -42,7 +43,19 @@ static int do_ice40_load(struct cmd_tbl *cmdtp, int flag, int argc, char * const
 	mdelay(2);
 
 	printf("XFER\n");
-	ret_val = spi_xfer(slave, len * 8, (void *)data, NULL, 0);
+	i = len * 8;
+	do {
+		if (i > 0x8000)
+			len = 0x8000;
+		else
+			len = i;
+		i -= len;
+
+		ret_val = spi_xfer(slave, len * 8, data, NULL, 0);
+		data += len;
+
+		printf("loop, rem %ld\n", i);
+	} while (i > 0 && ret_val == 0);
 	printf("XFER COMPLETE\n");
 
 	// FPGA requires additional spi clocks after bitstream
