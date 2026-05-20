06532d24c02739d514e13b2726015c3241b403a7
Kris Bahnsen <kris@embeddedTS.com>
2026-05-20 12:21:25 -0700

    ts4900: Add iCE40 programming routines for SPL use
    The TS-4900 implements an iCE40 FPGA that needs to be SRAM loaded
    on boot. This needs to happen before external DRAM is configured
    in order to read strap resistors connected to the FPGA that define
    the RAM configuration of the platform.
    
    For simplicity, the FPGA bitstream is stored in the SPI flash and
    loaded directly from that.
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

---
+++ board/embeddedts/ts4900/ice40.c
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2016-2026 Technologic Systems, Inc. dba embeddedTS
+ */
+
+#include <asm/gpio.h>
+#include <command.h>
+#include <linux/delay.h>
+#include <spi.h>
+#include <spi_flash.h>
+#include <stdlib.h>
+
+#define ICE40_BLK_SZ	0x1000
+
+int do_ice40_load(void)
+{
+	ulong i;
+	ulong len = CFG_ICE40_LEN;
+	u32 data = CFG_ICE40_START;
+	struct spi_slave *fpga_dev;
+	struct spi_flash *nor_dev;
+	int ret_val;
+	u8 buf[ICE40_BLK_SZ];
+
+	gpio_request(CFG_ICE40_FPGA_DONE, "ice40");
+	gpio_request(CFG_ICE40_FPGA_RESET, "ice40");
+	gpio_request(CFG_ICE40_CS, "ice40");
+
+	/* This is on bus 1, but, not on a real CS. We need to use a GPIO for
+	 * CS so we can manually control it as needed for programming.
+	 */
+	fpga_dev = spi_setup_slave(1, 0, 25000000, SPI_MODE_3);
+	ret_val = spi_claim_bus(fpga_dev);
+	if (ret_val)
+		goto out;
+
+	nor_dev = spi_flash_probe(CONFIG_SF_DEFAULT_BUS, CONFIG_SF_DEFAULT_CS,
+				  CONFIG_SF_DEFAULT_SPEED, CONFIG_SF_DEFAULT_MODE);
+	if (!nor_dev) {
+		ret_val = -ENODEV;
+		printf("Failed to probe bus\n");
+		goto spi_out;
+	}
+
+	gpio_direction_input(CFG_ICE40_FPGA_DONE); // fpga_done
+	gpio_direction_output(CFG_ICE40_FPGA_RESET, 0); // reset low
+	gpio_direction_output(CFG_ICE40_CS, 0); // spi cs# low
+	udelay(1); // at least 200ns
+	gpio_set_value(CFG_ICE40_FPGA_RESET, 1); // reset high
+	mdelay(2);
+
+	i = len;
+	do {
+		/* Calculate remaining length to read/write */
+		if (i > ICE40_BLK_SZ)
+			len = ICE40_BLK_SZ;
+		else
+			len = i;
+		i -= len;
+
+		/* Get data from SPI flash */
+		ret_val = spi_flash_read(nor_dev, data, ICE40_BLK_SZ, buf);
+		if (ret_val < 0) {
+			printf("SPI flash read failed\n");
+			goto spi_out;
+		} 
+
+		ret_val = spi_xfer(fpga_dev, len * 8, buf, NULL, 0);
+		if (ret_val) {
+			printf("SPI xfer to FPGA failed @ byte %x\n", data);
+			goto spi_out;
+		}
+		data += len;
+
+	} while (i > 0 && ret_val == 0);
+
+	// FPGA requires additional spi clocks after bitstream
+	memset(buf, 0, 100);
+	ret_val = spi_xfer(fpga_dev, 100 * 8, buf, NULL, 0);
+	if (ret_val) {
+		printf("SPI xfer to FPGA failed @ clocks\n");
+		goto spi_out;
+	}
+
+	gpio_set_value(CFG_ICE40_CS, 1); // spi cs# high
+
+	for(i = 0; i <= 3000; i++)
+	{
+		if(gpio_get_value(CFG_ICE40_FPGA_DONE)){
+			printf("iCE40 FPGA reloaded successfully\n");
+			break;
+		}
+		if(i == 3000){ 
+			printf("FPGA_DONE never asserted\n");
+			ret_val = -1;
+		}
+		udelay(1000);
+	}
+
+spi_out:
+	spi_release_bus(fpga_dev);
+
+out:
+	return ret_val;
+}
