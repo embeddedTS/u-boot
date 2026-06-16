edd77bf2c5ab16f5dc97061d3d63df8e4d703cf1
Kris Bahnsen <kris@embeddedTS.com>
2026-06-16 16:30:10 -0700

    embeddedts: Add baseboard detection logic
    More generic way to pull in baseboard detection on platforms
    by having a standalone API that can be passed arbitrary GPIO pins
    for the MUX input and output
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

---
+++ board/embeddedts/common/bbdetect.h
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2026 Technologic Systems, Inc. dba embeddedTS
+ */
+
+#ifndef BBDETECT_H
+#define BBDETECT_H
+
+#include <stddef.h>
+
+/* This shouldn't ever need to be configured differently per-platform but can
+ * be converted to a CFG_ option later if needed.
+ */
+#define BBID_MUX_BITS	3
+
+struct bbdetect_pins {
+	char *bit[BBID_MUX_BITS];
+	char *in;
+};
+
+int bbdetect(const struct bbdetect_pins pins, unsigned long delay);
+
+#endif // BBDETECT_H
