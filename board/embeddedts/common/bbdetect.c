bdb82bfcdba122384ae0f681fd6d8ad732708437
Kris Bahnsen <kris@embeddedTS.com>
2026-06-16 16:30:10 -0700

    embeddedts/common/bbdetect: Initial commit
    More generic way to pull in baseboard detection on platforms
    by having a standalone API that can be passed arbitrary GPIO pins
    for the MUX input and output
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

---
+++ board/embeddedts/common/bbdetect.c
@@ -0,0 +1,70 @@
+/* Generic bbdetect mechanism using embeddedTS MUX on baseboard with 3 bits of
+ * output selection, and then output.
+ */
+
+#include <asm/gpio.h>
+#include <env.h>
+#include <linux/delay.h>
+#include <stddef.h>
+
+#include "bbdetect.h"
+
+#if !CONFIG_IS_ENABLED(DM_GPIO)
+#error "The bbdetect functions require DM_GPIO!"
+#endif
+
+int bbdetect(const struct bbdetect_pins pins, unsigned long delay)
+{
+	struct gpio_desc desc_bit[BBID_MUX_BITS];
+	struct gpio_desc desc_in;
+	int i, bit;
+	u8 bbid = 0;
+	u8 id, rev;
+	int rc;
+
+	/* Request pins, setting outputs low */
+	for (i = 0; i < BBID_MUX_BITS; i++) {
+		if (gpio_request_by_line_name(NULL, pins.bit[i], &desc_bit[i],
+				(GPIOD_IS_OUT)) < 0)
+			return -1;
+	}
+	if (gpio_request_by_line_name(NULL, pins.in, &desc_in,
+			(GPIOD_IS_IN)) < 0)
+		return -1;
+
+	/* Shift in data */
+	for (i = 0; i < 8; i++) {
+		for (bit = 0; bit < BBID_MUX_BITS; bit++) {
+			if (dm_gpio_set_value(&desc_bit[bit], !!(i & (1 << bit))) < 0)
+				return -1;
+		}
+
+		udelay(delay);
+
+		bbid >>= 1;
+		rc = dm_gpio_get_value(&desc_in);
+		if (rc < 0)
+			return -1;
+		else if (rc > 0)
+			bbid |= 0x80;
+	}
+
+	/* Parse out strap segments from whole bbid */
+	id = (bbid & 0x3f);
+	rev = (bbid & 0xc0) >> 6;
+
+	env_set_hex("baseboard_id", id);
+	env_set_hex("baseboard_rev", rev);
+
+	/* Release pins */
+	/* BUG!!
+	 * dm_gpio_free() dereferences the first arg, which we don't
+	 * have a struct udevice due to how we obtained the GPIO, so,
+	 * this could be a problem, but is the "right thing" to do.
+	 */
+	dm_gpio_free(NULL, &desc_in);
+	for (i = 0; i < BBID_MUX_BITS; i++)
+		dm_gpio_free(NULL, &desc_bit[i]);
+
+	return 0;
+}
