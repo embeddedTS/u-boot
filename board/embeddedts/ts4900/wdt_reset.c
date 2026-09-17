d0ad3cffe942a465239d837a3a1d8481da8bcf1f
Kris Bahnsen <kris@embeddedTS.com>
2026-09-17 15:41:59 -0700

    ts4900: Provide strong reset_cpu() definition for WDT reset
    This uses the CPU WDT, mimics the behavior of
    imx_watchdog_expire_now() which is statically defined inside of
    imx_watchdog.c and otherwise not usable at runtime.
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

---
+++ board/embeddedts/ts4900/wdt_reset.c
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2026 Technologic Systems, Inc. dba embeddedTS
+ */
+
+#include <asm/arch/imx-regs.h>
+#include <asm/io.h>
+#include <watchdog.h>
+#include <fsl_wdog.h>
+
+/* This function behaves the same as imx_watchdog_expire_now(), but provides a
+ * non-weak reset_cpu() call.
+ */
+void reset_cpu(void)
+{
+	struct watchdog_regs *wdog = (struct watchdog_regs *)WDOG1_BASE_ADDR;
+
+	u16 wcr = WCR_WDE;
+
+	/* Asserts both internal and external WDOG resets */
+
+	/* Write 3 times to ensure it works, due to IMX6Q errata ERR004346 */
+	writew(wcr, &wdog->wcr);
+	writew(wcr, &wdog->wcr);
+	writew(wcr, &wdog->wcr);
+
+	while (1) {
+		 /*
+		  * spin before reset
+		  */
+	}
+}
---
+++ board/embeddedts/ts4900/wdt_reset.h
@@ -0,0 +1,6 @@
+#ifndef WDT_RESET_H
+#define WDT_RESET_H
+
+void reset_cpu(void);
+
+#endif // WDT_RESET_H
